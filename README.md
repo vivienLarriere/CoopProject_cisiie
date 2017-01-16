e241c2efc7c3fd2efb7c487c7f0c17b73cf79a6b
Hugo Torzuoli <torzuoli.hugo@gmail.com>
2017-01-16 09:39:17 +0100

    .

---
+++ README.md
@@ -0,0 +1,1 @@
+# CoopProject_cisiie
---
+++ resources/routes.js
@@ -0,0 +1,10 @@
+var app = angular.module("coop", ['ngRoute']);
+
+app.config(['$routeProvider', function ($routeProvider) {
+
+    $routeProvider.when('/signin', {
+
+        templateUrl: 'views/sign_in.html',
+        controller: 'SigninController'
+    });
+}]);
