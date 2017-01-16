09fc224f4c6ea536758aa49fde227b8b76b42bd5
vivienLarriere <vivien.larriere@gmail.com>
2017-01-16 09:33:48 +0100

    /signin and /members/:id ok

--- resources/routes.js
+++ resources/routes.js
@@ -3,7 +3,6 @@ var app = angular.module("coop", ['ngRoute']);
 app.config(['$routeProvider', function ($routeProvider) {
 
     $routeProvider.when('/signin', {
-
         templateUrl: 'views/sign_in.html',
         controller: 'SigninController'
     });
